cd3b96bbf249e25dd0a3b6844b77ecf58d6f62ed
Harrmalik <harrmalik@gmail.com>
2016-08-19 00:10:18 -0400

    adding color thief

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
 var img = document.createElement('img');
-img.setAttribute('src', '/imgs/coloring-book.jpeg');
-console.log(document.getElementById('chance'));
-console.log(img);
+img.setAttribute('src', '/imgs/title.png');
 img.addEventListener('load', function() {
     var vibrant = new Vibrant(img);
     var swatches = vibrant.swatches();
@@ -10,7 +8,25 @@ img.addEventListener('load', function() {
         //     console.log(swatch, swatches[swatch].getHex());
         // }
 
-    document.body.style.backgroundColor = swatches['Vibrant'].getHex();
-    document.body.style.color = swatches['LightVibrant'].getHex();
+    $(".v").css("color", swatches['Vibrant'].getHex());
+    $(".lv").css("color", swatches['LightVibrant'].getHex());
+    $(".m").css("color", swatches['Muted'].getHex());
+    $(".lm").css("color", swatches['DarkMuted'].getHex());
     //}
 });
+
+
+$(document).ready(function () {
+    var ct = new ColorThief();
+    console.log(ct.getColor(img));
+    color = ct.getColor(img, 8)
+    console.log(`rgb(${color[0]}, ${color[1]}, ${color[2]})`);
+    document.body.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    var pal = ct.getPalette(img, 8);
+    console.log(pal);
+    console.log(pal[0]);
+    console.log(pal[0][0]);
+    $('.1').css("color", `rgb(${pal[0][2]}, ${pal[0][0]}, ${pal[0][1]})`);
+    $('.2').css("color", `rgb(${pal[2][2]}, ${pal[0][0]}, ${pal[2][1]})`);
+    $('.3').css("color", `rgb(${pal[3][0]}, ${pal[0][1]}, ${pal[3][2]})`);
+})
